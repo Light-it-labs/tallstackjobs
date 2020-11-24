37e4aea1b79634dfea5aa7b6c30e8e878c372dbd
Juan <juan@buildonline.co>
2020-11-24 11:30:56 -0300

    Add fake jobs generation in database seeder

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Job;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JobFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Job::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->jobTitle,
+            'description' => $this->faker->text(200),
+            'currency' => 'USD',
+            'salary' => $this->faker->numberBetween(200, 800),
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
+}
