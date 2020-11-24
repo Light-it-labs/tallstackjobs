29ce798861361092dd62059dbd83f6d0bee66cc9
Juan <juan@buildonline.co>
2020-11-24 11:23:51 -0300

    Add job model and job migration

---
+++ database/migrations/2020_11_24_142153_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('email');
+            $table->double('salary');
+            $table->string('currency');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
