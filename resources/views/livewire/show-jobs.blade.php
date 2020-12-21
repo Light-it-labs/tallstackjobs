c907cb863aec0f6a1867c9115014967aa8ae9fe8
Walter <walter.gonzlz@gmail.com>
2020-12-21 16:32:41 -0300

    Display hashtags in view

--- resources/views/livewire/show-jobs.blade.php
+++ resources/views/livewire/show-jobs.blade.php
@@ -11,9 +11,19 @@
     
     <div class="bg-white min-w-0 flex-1 px-32 mb-5">
         <template x-for="(job, index) in {{ json_encode($jobs->toArray()['data']) }}" :key="index">
-            <div class="border-b border-gray-100 p-5 cursor-pointer" x-on:click="current = job; showModal = true">
-                <h2 class="font-medium text-xl" x-text="job.name"></h2>
-                <p x-text="job.description"></p>
+            <div class="flex justify-between border-b border-gray-100 p-5 cursor-pointer" x-on:click="current = job; showModal = true">
+                <img class="self-start" src="http://placehold.it/80x80.jpg" alt="">
+                <div class="flex-1 pl-3">
+                    <h2 class="font-medium text-xl" x-text="job.name"></h2>
+                    <p x-text="job.description"></p>
+                    <template x-for="(hashtag, index) in job.hashtags">
+                        <span x-text="hashtag.label" class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800"></span>
+                    </template>
+                    <br>
+                    <button type="button" class="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Apply
+                    </button>
+                </div>
             </div>
         </template>
     </div>
@@ -35,18 +45,22 @@
     
                 <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6" role="dialog" aria-modal="true" aria-labelledby="modal-headline" @click.away="showModal = false">
                 <div class="sm:flex sm:items-start">
-                    <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
-                        <p x-text="current.name"></p>
-                    </h3>
-                    <div class="mt-2">
-                        <p class="text-sm text-gray-500" x-text="current.description"></p>
-                    </div>
+                    <div class="flex flex-col sm:flex-row justify-between mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                        <img class="self-start" src="http://placehold.it/80x80.jpg" alt="">
+                        <div class="pl-3">
+                            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                                <p x-text="current.name"></p>
+                            </h3>
+                            <p class=" mt-2 text-sm text-gray-500" x-text="current.description"></p>
+                        </div>
                     </div>
                 </div>
                 <div class="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
                     <button @click="showModal = false" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
                     Close
+                    </button>
+                    <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                    Apply
                     </button>
                 </div>
                 </div>
