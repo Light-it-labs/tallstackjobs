3595e2c9bcb5218cf8cf03c1f56671d5c10829e9
Walter <walter.gonzlz@gmail.com>
2021-01-07 10:54:24 -0300

    Adds logo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -29,12 +29,12 @@
 
     <body>
         <div class="relative min-h-screen flex flex-col">
-            <nav class="flex-shrink-0 bg-indigo-600">
+            <nav class="flex-shrink-0 bg-white">
                 <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
                     <div class="relative flex items-center justify-between h-16">
                         <div class="flex items-center px-2 lg:px-0 xl:w-64">
                             <div class="flex-shrink-0">
-                                {{-- <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg" alt="Workflow"> --}}
+                                <img class="h-8 w-auto" src="images/tallstack-logo.svg" alt="Workflow">
                             </div>
                         </div>
             
@@ -42,11 +42,11 @@
                             <div class="flex items-center justify-end">
                                 <div class="flex">
                                     @auth
-                                        <a href="{{ route('jobs') }}" class="px-3 py-2 rounded-md text-sm font-medium text-indigo-200 hover:text-white">Jobs</a>
+                                        <a href="{{ route('jobs') }}" class="px-3 py-2 rounded-md text-sm font-medium text-indigo-400 hover:text-indigo-800">Jobs</a>
                                         <a
                                             href="{{ route('logout') }}"
                                             onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
-                                            class="px-3 py-2 rounded-md text-sm font-medium text-indigo-200 hover:text-white"
+                                            class="px-3 py-2 rounded-md text-sm font-medium text-indigo-400 hover:text-indigo-800"
                                         >
                                             Log out
                                         </a>
