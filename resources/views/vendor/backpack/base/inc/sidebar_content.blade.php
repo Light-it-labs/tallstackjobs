dc6fb16ac77337ca440c1910ac0c5fce9445788f
Walter <walter.gonzlz@gmail.com>
2021-01-07 15:40:12 -0300

    Changes icons for admin sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,5 +1,5 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('job') }}'><i class='nav-icon la la-question'></i> Jobs</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('company') }}'><i class='nav-icon la la-question'></i> Companies</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('hashtag') }}'><i class='nav-icon la la-question'></i> Hashtags</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('job') }}'><i class='nav-icon la la-suitcase'></i> Jobs</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('company') }}'><i class='nav-icon la la-building'></i> Companies</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('hashtag') }}'><i class='nav-icon la la-hashtag'></i> Hashtags</a></li>
