ab5e1c1f4f2f299480ebb6466965dd5e0c299996
Walter <walter.gonzlz@gmail.com>
2021-01-19 11:12:02 -0300

    Styles single job page template

--- resources/views/show-job.blade.php
+++ resources/views/show-job.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-full max-w-screen-sm mx-auto px-2 mb-8">
-    <div class="w-full flex justify-between border-b border-gray-200 last:border-b-0 px-2 md:px-0 py-5 cursor-pointer">
+<div class="w-full max-w-screen-sm px-2 mx-auto mb-8">
+    <div class="flex justify-between w-full px-2 py-5 border-b border-gray-200 cursor-pointer last:border-b-0 md:px-0">
         <img style="margin-top:5px;" class="self-start rounded" src="{{url('/' . $job->company->logo)}}" alt="">
         <div class="flex-1 pl-3">
-            <h2 class="font-medium text-xl">{{$job->name}}</h2>
+            <h2 class="text-xl font-medium">{{$job->name}}</h2>
             <p class="text-sm font-light">{{$job->description}}</p>
             @if (count($job->hashtags) > 0)
             <div class="mt-3">                    
@@ -15,7 +15,7 @@
             </div>
             @endif
             <a target="_blank" href="{{$job->apply_link}}"
-            class="inline-flex items-center px-2 py-1 mt-6 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            class="inline-flex items-center px-2 py-1 mt-6 text-xs font-medium text-white bg-indigo-600 border border-transparent rounded shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             Apply
             </a>
         </div>
